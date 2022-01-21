7e3ab3cf058465cdf31edd81a854d8016ef20156
Jenny <JennyTheKitty@protonmail.com>
2022-01-21 12:38:21 +0000

    Implement basic news

--- @app/client/src/pages/News.vue
+++ @app/client/src/pages/News.vue
@@ -1,3 +1,48 @@
 <template>
-  <span>News</span>
+  <main
+    role="main"
+    w:m="x-auto b-5 md:b-20 t-10"
+    w:max-w="5xl"
+    w:w="full"
+    w:text="true-gray-100"
+    w:flex="~ col"
+    w:align="items-center"
+    w:space="y-20"
+  >
+    <div
+      v-for="news in newses"
+      w:rounded="lg"
+      w:bg="dark-800"
+      w:w="full"
+      w:shadow="lg"
+      w:flex="~ col"
+      w:p="x-40 y-5"
+    >
+      <h1 w:text="3xl" w:font="tracking-wider" w:m="b-2">{{ news.title }}</h1>
+      <div class="content" w:text="base" v-html="news.content" />
+      <span w:text="sm" w:m="t-5">
+        <span w:font="light">Published</span>
+        {{ dayjs(news.publishedAt).format('LL') }}
+        <span v-if="news.updatedAt !== news.publishedAt">
+          <br />
+          <span w:font="light">Last updated: </span>
+          {{ dayjs(news.updatedAt).format('LL') }}
+        </span>
+      </span>
+    </div>
+  </main>
 </template>
+
+<script setup lang="ts">
+import { NewsesQueryDocument } from '@app/graphql/dist/client';
+import dayjs from 'dayjs';
+
+const { t, locale } = useI18n();
+
+const { data } = useQuery({
+  query: NewsesQueryDocument,
+});
+const newses = computed(() =>
+  (data.value?.newses?.nodes || []).map((news) => useTranslation(news, locale))
+);
+</script>
